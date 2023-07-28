24168bd32e2d037d828fe788f9b65627a807cc61
yamasakiyamasaki <exactly.a.y@docomo.ne.jp>
2023-07-28 16:06:35 +0900

    shuusei

--- sukinakotopage/sample.sql
+++ sukinakotopage/sample.sql
@@ -20,6 +20,8 @@ SET time_zone = "+00:00";
 --
 -- データベース: `avengers`
 --
+CREATE DATABASE IF NOT EXISTS `avengers` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `avengers`;
 
 -- --------------------------------------------------------
 
